d6f90cf8766b6fd31d5b294c9b0b8bcb995f0c2f
Gustavo Vieira <gustavieiradev@gmail.com>
2025-05-03 13:41:34 -0300

    Adicionando personagem ao Jogo e criando a primeira ideia de movimentação

---
+++ ZumbiGame/Assets/ControlaJogador.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class ControlaJogador : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.forward);
+    }
+}
